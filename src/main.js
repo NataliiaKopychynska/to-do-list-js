9f6b8887381482af7e24cfa6180b975086196176
Nataliia <nkopichinckaya@gmail.com>
2025-02-21 09:46:13 +0100

    add all

--- src/main.js
+++ src/main.js
@@ -29,10 +29,10 @@ document.addEventListener('DOMContentLoaded', () => {
   filterTasks(savedFilter);
 
   if (localStorage.getItem('theme') === 'dark') {
-    svgUse.setAttribute('href', '/img/symbol-defs.svg#icon-sun');
+    svgUse.setAttribute('href', 'img/symbol-defs.svg#icon-sun');
     darkTheme();
   } else {
-    svgUse.setAttribute('href', '/img/symbol-defs.svg#icon-moon');
+    svgUse.setAttribute('href', 'img/symbol-defs.svg#icon-moon');
     lightTheme();
   }
 });
@@ -52,11 +52,11 @@ modeToggle.addEventListener('click', () => {
   document.body.classList.toggle('dark-theme');
 
   if (document.body.classList.contains('dark-theme')) {
-    svgUse.setAttribute('href', '/img/symbol-defs.svg#icon-sun');
+    svgUse.setAttribute('href', 'img/symbol-defs.svg#icon-sun');
     localStorage.setItem('theme', 'dark');
     darkTheme();
   } else {
-    svgUse.setAttribute('href', '/img/symbol-defs.svg#icon-moon');
+    svgUse.setAttribute('href', 'img/symbol-defs.svg#icon-moon');
     localStorage.setItem('theme', 'light');
     lightTheme();
   }
