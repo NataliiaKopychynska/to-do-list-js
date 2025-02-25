8ed564d4abd842d63bfd8b3fe96279dfb202c82e
NataliiaKopychynska <nkopichinckaya@gmail.com>
2025-02-25 19:26:44 +0000

    Deploying to gh-pages from @ NataliiaKopychynska/to-do-list-js@1af71771f889671e371d67f29275816595afb712 🚀

--- index.js
+++ index.js
@@ -19,7 +19,7 @@
             <div class="btn-container-add-delete">
               <button class="btn-list btn-edid">
                 <svg class="svg-list">
-                  <use href="../assets/edit.svg"></use>
+                  <use href="/img/sprite.svg#icon-edit"></use>
                 </svg></button
               ><button class="btn-list btn-delete">
                 <svg class="svg-list">
